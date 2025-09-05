de3789c4c1cae7cd7bc6b90d85a4e4720e974708
Parshwa0409 <parshwapail9@gmail.com>
2025-09-05 13:58:58 +0530

    feat: updating dto, to mimic old requests

--- expenseTracker/src/main/java/com/parshwa/expenseTracker/dto/CategorySummaryDto.java
+++ expenseTracker/src/main/java/com/parshwa/expenseTracker/dto/CategorySummaryDto.java
@@ -6,51 +6,16 @@ import lombok.Setter;
 // This will be used for pie-chart, progress-bar, category-summary
 @Getter @Setter
 public class CategorySummaryDto {
+    int categoryId;
     private String emoji;
     private String name;
     private long totalExpense;
     private long budget;
 
-    //org.springframework.core.convert.ConverterNotFoundException: No converter found capable of converting from type [java.lang.String] to type [com.parshwa.expenseTracker.model.CategoryUtilizationDto]
-    public CategorySummaryDto(String emoji, String name, long totalExpense, long budget) {
+    public CategorySummaryDto(int categoryId, String emoji, String name, long totalExpense) {
+        this.categoryId = categoryId;
         this.emoji = emoji;
         this.name = name;
         this.totalExpense = totalExpense;
-        this.budget = budget;
     }
-
-    // com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class ...
-    /*
-    public String getEmoji() {
-        return emoji;
-    }
-
-    public void setEmoji(String emoji) {
-        this.emoji = emoji;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getTotalExpense() {
-        return totalExpense;
-    }
-
-    public void setTotalExpense(long totalExpense) {
-        this.totalExpense = totalExpense;
-    }
-
-    public long getBudget() {
-        return budget;
-    }
-
-    public void setBudget(long budget) {
-        this.budget = budget;
-    }
-    */
 }
--- expenseTracker/src/main/java/com/parshwa/expenseTracker/dto/ExpenseWithCategoryDto.java
+++ expenseTracker/src/main/java/com/parshwa/expenseTracker/dto/ExpenseWithCategoryDto.java
@@ -1,5 +1,6 @@
 package com.parshwa.expenseTracker.dto;
 
+import com.parshwa.expenseTracker.model.Category;
 import com.parshwa.expenseTracker.model.Expense;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -23,22 +24,14 @@ public class ExpenseWithCategoryDto {
     @NotNull(message = "Date of Expense cannot be null")
     private LocalDate date;
 
-    @Min(value = 1, message = "Category is required")
-    private int categoryId;
-
-    public ExpenseWithCategoryDto(int id, String title, int amount, LocalDate date, int categoryId) {
-        this.id = id;
-        this.title = title;
-        this.amount = amount;
-        this.date = date;
-        this.categoryId = categoryId;
-    }
+    @NotNull(message = "Expense must have a Category")
+    private Category category;
 
     public ExpenseWithCategoryDto(Expense expense) {
         this.id = expense.getId();
         this.title = expense.getTitle();
         this.amount = expense.getAmount();
         this.date = expense.getDate();
-        this.categoryId = expense.getCategory().getId();
+        this.category = expense.getCategory();
     }
 }
