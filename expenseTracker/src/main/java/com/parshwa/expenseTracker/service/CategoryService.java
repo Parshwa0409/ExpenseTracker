0a88dee2411ccc871d528e6dec89a5f97aaab15f
Parshwa0409 <parshwapail9@gmail.com>
2025-09-06 10:07:25 +0530

    cleanup: removed unsed import

--- expenseTracker/src/main/java/com/parshwa/expenseTracker/service/CategoryService.java
+++ expenseTracker/src/main/java/com/parshwa/expenseTracker/service/CategoryService.java
@@ -6,7 +6,6 @@ import com.parshwa.expenseTracker.model.Category;
 import com.parshwa.expenseTracker.repository.CategoryRepository;
 import jakarta.transaction.Transactional;
 import org.apache.coyote.BadRequestException;
-import org.hibernate.query.IllegalQueryOperationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
